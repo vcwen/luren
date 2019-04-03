882237a0acd5aa91030478cae09a9acaa9fbd297
Vincent Wen <wenwei1202@gmail.com>
2019-04-03 10:42:17 +0800

    refactor(luren): move some funcs to helper

--- src/Luren.ts
+++ src/Luren.ts
@@ -1,23 +1,14 @@
-import Ajv from 'ajv'
-import Boom from 'boom'
-import Debug from 'debug'
 import { List, Map } from 'immutable'
 import { Container } from 'inversify'
 import Koa from 'koa'
-import Router, { IMiddleware, IRouterContext } from 'koa-router'
+import Router, { IRouterContext } from 'koa-router'
 import _ from 'lodash'
 import { Server } from 'net'
 import Path from 'path'
-import { HttpStatusCode, MetadataKey } from './constants'
 import { ServiceIdentifier } from './constants/ServiceIdentifier'
 import { IDatasource } from './datasource/LurenDatasource'
-import { ParamMetadata, ResponseMetadata, RouteMetadata } from './decorators'
-import { getParams } from './lib/Helper'
-import { HttpStatus } from './lib/HttpStatus'
+import { createController } from './lib/helper'
 import { getFileLoaderConfig, importModules } from './lib/utils'
-import { parseFormData, transform } from './lib/utils'
-const debug = Debug('luren')
-const ajv = new Ajv()
 
 export interface IModuleLoaderConfig {
   path: string
@@ -45,7 +36,7 @@ export class Luren {
   private _controllerConfig?: IModuleLoaderConfig
   private _modelConfig?: IModuleLoaderConfig
   private _datasource: Map<string, IDatasource> = Map()
-  private _onError?: (err: any) => void
+  private _onError?: (err: any, ctx: IRouterContext) => void
   constructor(options?: {
     container?: Container
     bootOptions?: IModuleLoaderOptions
@@ -71,7 +62,7 @@ export class Luren {
   public getPrefix() {
     return this._prefix
   }
-  public onError(onError: (err: any) => void) {
+  public onError(onError: (err: any, ctx: IRouterContext) => void) {
     this._onError = onError
   }
   public setWorkDirectory(dir: string) {
@@ -90,9 +81,6 @@ export class Luren {
     } catch (err) {
       throw err
     }
-  }
-  public getRouter() {
-    return this._router
   }
 
   public getKoa() {
@@ -125,57 +113,13 @@ export class Luren {
       plugin(this)
     }
   }
-  public _createAction(controller: object, propKey: string) {
-    const paramsMetadata: List<ParamMetadata> =
-      Reflect.getOwnMetadata(MetadataKey.PARAMS, Reflect.getPrototypeOf(controller), propKey) || List()
-    const action = async (ctx: IRouterContext, next?: any) => {
-      try {
-        if (!ctx.disableFormParser && ctx.is('multipart/form-data')) {
-          const { fields, files } = await parseFormData(ctx)
-          const request: any = ctx.request
-          request.body = fields
-          request.files = files
-        }
-        const args = getParams(ctx, paramsMetadata)
-        await this._processRoute(ctx, controller, propKey, args.toArray())
-        if (next) {
-          await next()
-        }
-      } catch (err) {
-        if (Boom.isBoom(err)) {
-          if (err.isServer) {
-            throw err
-          } else {
-            ctx.status = err.output.statusCode
-            const response = !_.isEmpty(err.output.payload.attributes)
-              ? err.output.payload.attributes
-              : err.output.payload.message
-            const resultMetadataMap: Map<number, ResponseMetadata> =
-              Reflect.getMetadata(MetadataKey.RESPONSE, controller, propKey) || Map()
-            const resMetadata = resultMetadataMap.get(err.output.statusCode)
-            if (resMetadata && resMetadata.strict) {
-              if (ajv.validate(resMetadata.schema, response)) {
-                ctx.body = transform(response, resMetadata.schema, resMetadata.schema)
-              } else {
-                ctx.body = response
-              }
-            } else {
-              ctx.body = response
-            }
-          }
-        } else {
-          throw err
-        }
-      }
-    }
-    return action
-  }
+
   private async _initialize() {
     await this._loadModelModules()
     await this._loadMiddleware()
     const router = this._router
     await this._loadControllerModules()
-    this._loadControllers(this, this._controllers)
+    this._loadControllers()
     this._koa.use(router.routes()).use(router.allowedMethods())
   }
   private async _loadMiddleware() {
@@ -251,91 +195,10 @@ export class Luren {
       }
     }
   }
-  private _applyCtrlMiddleware(router: Router, middleware: List<IMiddleware>) {
-    middleware.forEach((mw) => {
-      router.use(mw)
+  private _loadControllers() {
+    this._controllers.forEach((item) => {
+      const ctrl = createController(item, this._onError)
+      this._router.use(ctrl.routes(), ctrl.allowedMethods())
     })
-  }
-  private _processRoute = async (ctx: IRouterContext, controller: any, propKey: string, args: any[]) => {
-    const response = await controller[propKey].apply(controller, args)
-    if (response instanceof HttpStatus) {
-      ctx.status = response.statusCode
-      switch (response.statusCode) {
-        case HttpStatusCode.MOVED_PERMANENTLY:
-        case HttpStatusCode.FOUND:
-          return ctx.redirect(response.body)
-        default:
-          ctx.body = response.body
-      }
-    } else {
-      const resultMetadataMap: Map<number, ResponseMetadata> =
-        Reflect.getMetadata(MetadataKey.RESPONSE, controller, propKey) || Map()
-      const resMetadata = resultMetadataMap.get(HttpStatusCode.OK)
-      if (resMetadata && resMetadata.strict) {
-        if (ajv.validate(resMetadata.schema, response)) {
-          ctx.body = transform(response, resMetadata.schema, resMetadata.schema)
-        } else {
-          throw new Error(ajv.errorsText())
-        }
-      } else {
-        ctx.body = response
-      }
-    }
-  }
-
-  private _createRoute(controller: object, propKey: string, routeMetadata: RouteMetadata) {
-    const action = this._createAction(controller, propKey)
-    const middleware: List<IMiddleware> = Reflect.getMetadata(MetadataKey.MIDDLEWARE, controller, propKey) || List()
-    return {
-      method: routeMetadata.method.toLowerCase(),
-      path: routeMetadata.path,
-      action,
-      middleware
-    }
-  }
-
-  private _createRoutes(controller: object) {
-    const routeMetadataMap: Map<string, RouteMetadata> = Reflect.getMetadata(MetadataKey.ROUTES, controller)
-    return routeMetadataMap
-      .map((routeMetadata, prop) => {
-        return this._createRoute(controller, prop, routeMetadata)
-      })
-      .toList()
-  }
-  private _createController(ctrl: object) {
-    const router: any = new Router()
-    const ctrlMiddleware: List<IMiddleware> = Reflect.getMetadata(MetadataKey.MIDDLEWARE, ctrl) || List()
-    this._applyCtrlMiddleware(router, ctrlMiddleware)
-    const routes = this._createRoutes(ctrl)
-    routes.forEach((route) => {
-      const action = async (ctx: Router.IRouterContext, next?: any) => {
-        try {
-          await route.action(ctx, next)
-        } catch (err) {
-          debug(err)
-          ctx.status = HttpStatusCode.INTERNAL_SERVER_ERROR
-          if (this._onError) {
-            this._onError(err)
-          } else {
-            // tslint:disable-next-line:no-console
-            console.error(err)
-          }
-        }
-      }
-      if (!route.middleware.isEmpty()) {
-        router[route.method](route.path, ...route.middleware, action)
-      } else {
-        router[route.method](route.path, action)
-      }
-    })
-    return router
-  }
-  private _loadControllers = (luren: Luren, controllers: List<object>) => {
-    const router = luren.getRouter()
-    controllers.forEach((item) => {
-      const ctrl = this._createController(item)
-      router.use(ctrl.routes(), ctrl.allowedMethods())
-    })
-    return router
   }
 }
--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -1,10 +1,19 @@
 import Ajv from 'ajv'
 import Boom from 'boom'
-import { List } from 'immutable'
-import { IRouterContext } from 'koa-router'
+import Debug from 'debug'
+import { List, Map } from 'immutable'
+import { IMiddleware, IRouterContext } from 'koa-router'
+import Router from 'koa-router'
+import _ from 'lodash'
 import 'reflect-metadata'
+import { MetadataKey } from '../constants'
 import { HttpStatusCode } from '../constants/HttpStatusCode'
+import { ResponseMetadata, RouteMetadata } from '../decorators'
 import { ParamMetadata } from '../decorators/Param'
+import { HttpStatus } from './HttpStatus'
+import { parseFormData, transform } from './utils'
+
+const debug = Debug('luren')
 
 const ajv = new Ajv()
 
@@ -64,4 +73,131 @@ export const getParams = (ctx: IRouterContext, paramsMetadata: List<ParamMetadat
     }
     return value
   })
+}
+
+export function applyCtrlMiddleware(router: Router, middleware: List<IMiddleware>) {
+  middleware.forEach((mw) => {
+    router.use(mw)
+  })
+}
+
+export async function processRoute(ctx: IRouterContext, controller: any, propKey: string, args: any[]) {
+  const response = await controller[propKey].apply(controller, args)
+  if (response instanceof HttpStatus) {
+    ctx.status = response.statusCode
+    switch (response.statusCode) {
+      case HttpStatusCode.MOVED_PERMANENTLY:
+      case HttpStatusCode.FOUND:
+        return ctx.redirect(response.body)
+      default:
+        ctx.body = response.body
+    }
+  } else {
+    const resultMetadataMap: Map<number, ResponseMetadata> =
+      Reflect.getMetadata(MetadataKey.RESPONSE, controller, propKey) || Map()
+    const resMetadata = resultMetadataMap.get(HttpStatusCode.OK)
+    if (resMetadata && resMetadata.strict) {
+      if (ajv.validate(resMetadata.schema, response)) {
+        ctx.body = transform(response, resMetadata.schema, resMetadata.schema)
+      } else {
+        throw new Error(ajv.errorsText())
+      }
+    } else {
+      ctx.body = response
+    }
+  }
+}
+
+export function createAction(controller: object, propKey: string) {
+  const paramsMetadata: List<ParamMetadata> =
+    Reflect.getOwnMetadata(MetadataKey.PARAMS, Reflect.getPrototypeOf(controller), propKey) || List()
+  const action = async (ctx: IRouterContext, next?: any) => {
+    try {
+      if (!ctx.disableFormParser && ctx.is('multipart/form-data')) {
+        const { fields, files } = await parseFormData(ctx)
+        const request: any = ctx.request
+        request.body = fields
+        request.files = files
+      }
+      const args = getParams(ctx, paramsMetadata)
+      await processRoute(ctx, controller, propKey, args.toArray())
+      if (next) {
+        await next()
+      }
+    } catch (err) {
+      if (Boom.isBoom(err)) {
+        if (err.isServer) {
+          throw err
+        } else {
+          ctx.status = err.output.statusCode
+          const response = !_.isEmpty(err.output.payload.attributes)
+            ? err.output.payload.attributes
+            : err.output.payload.message
+          const resultMetadataMap: Map<number, ResponseMetadata> =
+            Reflect.getMetadata(MetadataKey.RESPONSE, controller, propKey) || Map()
+          const resMetadata = resultMetadataMap.get(err.output.statusCode)
+          if (resMetadata && resMetadata.strict) {
+            if (ajv.validate(resMetadata.schema, response)) {
+              ctx.body = transform(response, resMetadata.schema, resMetadata.schema)
+            } else {
+              ctx.body = response
+            }
+          } else {
+            ctx.body = response
+          }
+        }
+      } else {
+        throw err
+      }
+    }
+  }
+  return action
+}
+
+export function createRoute(controller: object, propKey: string, routeMetadata: RouteMetadata) {
+  const action = createAction(controller, propKey)
+  const middleware: List<IMiddleware> = Reflect.getMetadata(MetadataKey.MIDDLEWARE, controller, propKey) || List()
+  return {
+    method: routeMetadata.method.toLowerCase(),
+    path: routeMetadata.path,
+    action,
+    middleware
+  }
+}
+
+export function createRoutes(controller: object) {
+  const routeMetadataMap: Map<string, RouteMetadata> = Reflect.getMetadata(MetadataKey.ROUTES, controller)
+  return routeMetadataMap
+    .map((routeMetadata, prop) => {
+      return createRoute(controller, prop, routeMetadata)
+    })
+    .toList()
+}
+export function createController(ctrl: object, onError?: (err: any, ctx: IRouterContext) => void) {
+  const router: any = new Router()
+  const ctrlMiddleware: List<IMiddleware> = Reflect.getMetadata(MetadataKey.MIDDLEWARE, ctrl) || List()
+  applyCtrlMiddleware(router, ctrlMiddleware)
+  const routes = createRoutes(ctrl)
+  routes.forEach((route) => {
+    const action = async (ctx: Router.IRouterContext, next?: any) => {
+      try {
+        await route.action(ctx, next)
+      } catch (err) {
+        debug(err)
+        ctx.status = HttpStatusCode.INTERNAL_SERVER_ERROR
+        if (onError) {
+          onError(err, ctx)
+        } else {
+          // tslint:disable-next-line:no-console
+          console.error(err)
+        }
+      }
+    }
+    if (!route.middleware.isEmpty()) {
+      router[route.method](route.path, ...route.middleware, action)
+    } else {
+      router[route.method](route.path, action)
+    }
+  })
+  return router
 }
