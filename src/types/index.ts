0d32a95d72f51838202d390a9defa8be0b9be9a4
Vincent Wen <wenwei1202@gmail.com>
2019-07-07 10:17:03 +0800

    refactor(type): add new types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,18 @@
+import { List } from 'immutable'
+import { Context, Middleware } from 'koa'
+
 export type IProcess = (...args: any[]) => Promise<any>
+export type INext = () => Promise<any>
+export type IAuthenticate = (...args: any[]) => Promise<boolean>
+export type IAuthorize = (...args: any[]) => Promise<boolean>
+export interface ISecuritySettings {
+  authentication?: Middleware
+  authorization?: List<Middleware>
+}
+
+export interface IMiddlewareAdaptable<T = any> {
+  process(...args: any[]): Promise<T>
+  toMiddleware(): Middleware
+}
+export type IValidationMiddleware = (context: Context, next: () => Promise<any>) => Promise<boolean> | boolean
+export type IMiddlewareConditions = { [key in 'and' | 'or']: Array<IValidationMiddleware | IMiddlewareConditions> }
