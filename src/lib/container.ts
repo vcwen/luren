83b77376f82c54011328139944855ef09daa378b
vincent.wen <wenwei1202@gmail.com>
2019-12-31 10:17:15 +0800

    refactor(inversify): export getContainer method

--- src/lib/container.ts
+++ src/lib/container.ts
@@ -1,4 +1,7 @@
 import { Container } from 'inversify'
 import 'reflect-metadata'
 export const container = new Container({ skipBaseClassChecks: true })
+export const getContainer = () => {
+  return container
+}
 export default container
